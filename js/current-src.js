9f4489921b74c3102033c73a01213d2027deaba6
Oscar Sobrevilla <oscar.sobrevilla@gmail.com>
2013-11-14 13:30:19 -0500

    update

--- js/current-src.js
+++ js/current-src.js
@@ -1,15 +1,18 @@
-window.onload = function(){
-    var hud = document.createElement('div');
+(function (window, document) {
+    'use strict';
+    window.onload = function () {
+        var hud = document.createElement('div'),
+            img = document.getElementsByTagName('img')[0],
+            checkSrc = function () {
+                hud.innerHTML = img.getAttribute('src') + '<br>(' + (img.naturalWidth + 'x' + img.naturalHeight) + ') Viewport: ' + (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
+            };
         hud.id = "current-src";
-    var img = document.getElementsByTagName('img')[0],
-        checkSrc = function(){ 
-            hud.innerHTML = img.getAttribute('src') + '<br>('+(img.naturalWidth + 'x' + img.naturalHeight)+') Viewport: ' + (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
-        };
-    if ( window.addEventListener ){
-        window.addEventListener('resize', checkSrc, false);
-    } else {
-        window.attachEvent('onresize', checkSrc);
-    }
-    document.body.appendChild(hud);
-    checkSrc();
-};
+        if (window.addEventListener) {
+            window.addEventListener('resize', checkSrc, false);
+        } else {
+            window.attachEvent('onresize', checkSrc);
+        }
+        document.body.appendChild(hud);
+        checkSrc();
+    };
+}(this, this.document));
